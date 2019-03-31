2a612f9c5fabc8c6ca3081ccdbfa9c9649d58a54
wk1102 <wk572896690@163.com>
2019-03-31 18:05:15 +0800

    客户端

---
+++ eureka-client/src/main/java/com/example/eurekaclient/EurekaClientApplication.java
@@ -0,0 +1,22 @@
+package com.example.eurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class EurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientApplication.class, args);
+	}
+    @GetMapping("get")
+	public String get(){
+		return "get方法";
+	}
+}
